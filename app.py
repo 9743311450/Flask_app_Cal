2a7ffdcc04bf42ef73e7d8967d4535a4df5ccbd0
9743311450 <84521451+9743311450@users.noreply.github.com>
2021-11-21 18:16:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,6 @@
-#!/usr/bin/env python
-from flask import Flask, request
+from flask import Flask
+app = Flask(__name__)
 
-# create app
-app = Flask(_name_)
-
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'GET':
-        # show html form
-        return '''
-            <form method="post">
-                <input type="text" name="expression" />
-                <input type="submit" value="Calculate" />
-            </form>
-        '''
-    elif request.method == 'POST':
-        # calculate result
-        expression = request.form.get('expression')
-        result = eval(expression)
-        return 'result: %s' % result
-
-# run app
-if _name_ == '_main_':
-    app.run(debug=True)
+@app.route("/")
+def hello():
+  return "Hello, World!"
